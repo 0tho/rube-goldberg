284d569ce35ab545607be22d4a89d8155bbcbf46
Raphael Gianotti Serrano dos Santos <rapha.gianotti@hotmail.com>
2014-10-28 23:06:22 -0200

    Initial positions of objects, reading input and moving plate

--- RubeGoldbergSimulation/src/rubegoldbergsimulation/RubeGoldbergSimulation.java
+++ RubeGoldbergSimulation/src/rubegoldbergsimulation/RubeGoldbergSimulation.java
@@ -14,88 +14,166 @@ import javax.media.j3d.*;
 import javax.vecmath.*;
 import com.sun.j3d.loaders.objectfile.ObjectFile;
 import com.sun.j3d.loaders.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import static java.lang.System.exit;
+import javax.swing.Timer;
 
-public class RubeGoldbergSimulation extends Applet { 
-        public final static String assetsFolder = "assets\\";
-    	SimpleUniverse simpleU; 
-    	static boolean application = false;
-	
-	public RubeGoldbergSimulation (){    
-	}    
+public class RubeGoldbergSimulation extends Applet implements ActionListener, KeyListener  {
 
-	public void init() { 
-      
-	setLayout(new BorderLayout()); 
-	Canvas3D c = new Canvas3D(SimpleUniverse.getPreferredConfiguration()); 
-    	add("Center", c);    
-	simpleU= new SimpleUniverse(c); // setup the SimpleUniverse, attach the Canvas3D
-    	BranchGroup scene = createSceneGraph(); 
-        simpleU.getViewingPlatform().setNominalViewingTransform();
-        scene.compile(); 
-        simpleU.addBranchGraph(scene); //add your SceneGraph to the SimpleUniverse   
+    public final static String assetsFolder = "src\\art_assets\\";
+    SimpleUniverse simpleU;
+    
+    TransformGroup appleTG = new TransformGroup();
+    TransformGroup plateTG = new TransformGroup();
+    TransformGroup balloonTG = new TransformGroup();
+    Transform3D appleT3D = new Transform3D();
+    Transform3D plateT3D = new Transform3D();
+    Transform3D balloonT3D = new Transform3D();
+    
+    Vector3d platePos = new Vector3d();
+    
+    private Timer timer;
+
+    public RubeGoldbergSimulation() {
     }
 
-    public BranchGroup createSceneGraph() {      
+    @Override
+    public void init() {
+        setLayout(new BorderLayout());
+        Canvas3D c = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
+        c.addKeyListener(this);
+        add("Center", c);
+        simpleU = new SimpleUniverse(c); // setup the SimpleUniverse, attach the Canvas3D
+        BranchGroup scene = createSceneGraph();
+        simpleU.getViewingPlatform().setNominalViewingTransform();
+        scene.compile();
+        simpleU.addBranchGraph(scene); //add your SceneGraph to the SimpleUniverse   
+    }
+    
+    private void setInitialPositions()
+    {
+        appleT3D.setScale(new Vector3d(.05, .05, .05));
+        appleT3D.setTranslation(new Vector3d(-.5, .3, 0));
+        appleTG.setTransform(appleT3D);
+        
+        plateT3D.setScale(new Vector3d(.2, .2, .2));
+        plateT3D.setTranslation(new Vector3d(-.5, 0, 0));
+        platePos = new Vector3d(-.5, 0, 0);
+        plateTG.setTransform(plateT3D);
+        
+        balloonT3D.setScale(new Vector3d(1.5, 1, 1));
+        balloonT3D.setTranslation(new Vector3d(.5, .25, 0));
+        balloonT3D.setRotation(new AxisAngle4f(new Vector3f(1, 0, 0), 180));
+        balloonTG.setTransform(balloonT3D);
+        
+        //no animation state
+        timer.stop();
+    }
 
-   	BranchGroup objRoot = new BranchGroup(); 
-	TransformGroup tg = new TransformGroup();
-        Transform3D t3d = new Transform3D();
-      	try
-	{	
-		Scene s = null;
-   		ObjectFile f = new ObjectFile ();
-    		f.setFlags (ObjectFile.RESIZE | ObjectFile.TRIANGULATE | ObjectFile.STRIPIFY);
+    public BranchGroup createSceneGraph() {
 
-                String objPath = assetsFolder + "ballon.obj";
-		if (application == false){
-			java.net.URL airFile = new java.net.URL (getCodeBase(), objPath);
-			s = f.load (airFile);
-			tg.addChild (s.getSceneGroup ());
-	
-		}
-		else {
-			s = f.load (objPath);
-			tg.addChild (s.getSceneGroup ());
-		}
+        BranchGroup objRoot = new BranchGroup();
+        appleTG = new TransformGroup();
+        plateTG = new TransformGroup();
+        balloonTG = new TransformGroup();
+        
+        appleTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        plateTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        balloonTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        
+        appleT3D = new Transform3D();
+        plateT3D = new Transform3D();
+        balloonT3D = new Transform3D();
+        try {
+            
+            Scene appleScene = null;
+            Scene plateScene = null;
+            Scene balloonScene = null;
+            
+            ObjectFile appleFile = new ObjectFile();
+            ObjectFile plateFile = new ObjectFile();
+            ObjectFile balloonFile = new ObjectFile();
+            
+            String applePath = assetsFolder + "apple.obj";
+            String platePath = assetsFolder + "plate.obj";
+            String balloonPath = assetsFolder + "balloon.obj";
+            
+            appleFile.setFlags(ObjectFile.RESIZE | ObjectFile.TRIANGULATE | ObjectFile.STRIPIFY);
+            plateFile.setFlags(ObjectFile.RESIZE | ObjectFile.TRIANGULATE | ObjectFile.STRIPIFY);
+            balloonFile.setFlags(ObjectFile.RESIZE | ObjectFile.TRIANGULATE | ObjectFile.STRIPIFY);
+            
+            appleScene = appleFile.load(applePath);
+            plateScene = plateFile.load(platePath);
+            balloonScene = balloonFile.load(balloonPath);
+            
+            appleTG.addChild(appleScene.getSceneGroup());
+            plateTG.addChild(plateScene.getSceneGroup());
+            balloonTG.addChild(balloonScene.getSceneGroup());
 
-	}
-
-      	catch (java.net.MalformedURLException ex){
+        } catch (java.io.FileNotFoundException ex) {
             System.err.println(ex);
             exit(1);
-      	}
-      	catch (java.io.FileNotFoundException ex){
-            System.err.println(ex);
-            exit(1);
-      	}
+        }
 
+        BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
 
-      	BoundingSphere bounds = new BoundingSphere (new Point3d (0.0, 0.0, 0.0), 100.0);
+        Color3f ambientColor = new Color3f(0.5f, 0.5f, 0.5f);
 
-      	Color3f ambientColor = new Color3f (0.5f, 0.5f, 0.5f);
+        AmbientLight ambientLightNode = new AmbientLight(ambientColor);
 
-      	AmbientLight ambientLightNode = new AmbientLight (ambientColor);
+        ambientLightNode.setInfluencingBounds(bounds);
 
-      	ambientLightNode.setInfluencingBounds (bounds);
+        objRoot.addChild(ambientLightNode);
+        
+        timer = new Timer(100, this);
+        
+        objRoot.addChild(appleTG);
+        objRoot.addChild(plateTG);
+        objRoot.addChild(balloonTG);
+        
+        setInitialPositions();
+        
+        return objRoot;
+    }
 
-      	objRoot.addChild (ambientLightNode);	
+    public void destroy() {
+        simpleU.removeAllLocales();
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //animation logic goes here
+        platePos = new Vector3d(platePos.x + 1.0 / timer.getDelay(), platePos.y, platePos.z);
+        plateT3D.setTranslation(platePos);
+        plateTG.setTransform(plateT3D);
+    }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //Do nothing
+    }
 
-      	t3d.setTranslation(new Vector3f(0f,0f,-2f));
-        t3d.setScale(new Vector3d(5f,5f,5f));
-        t3d.setRotation(new AxisAngle4f(new Vector3f(1, 0, 0), 270));
-      	tg.setTransform(t3d);
-      	objRoot.addChild(tg);
-      	return objRoot;
-      	}
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_S)
+        {
+            if(timer.isRunning())
+                setInitialPositions();
+            else
+                timer.start();
+        }
+    }
 
-      	public void destroy() {	
-		simpleU.removeAllLocales();    
-      	}  
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //Do nothing
+    }
 
-      	public static void main(String[] args) {
-		application = true;    
-        	Frame frame = new MainFrame(new RubeGoldbergSimulation(), 500, 500);    
-      	}
+    public static void main(String[] args) {
+        RubeGoldbergSimulation rgs = new RubeGoldbergSimulation();
+        MainFrame frame = new MainFrame(rgs, 1000, 800);
+    }
 }
